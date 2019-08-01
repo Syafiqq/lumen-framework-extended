91e191099dbe13601db0503ac0c8be38ade33df1
Syafiqq <syafiq.rezpector@gmail.com>
2019-08-01 14:21:54 +0700

    add configurationIsCached

--- src/Application.php
+++ src/Application.php
@@ -6,5 +6,12 @@ namespace Syafiqq\Lumen;
 
 class Application extends \Laravel\Lumen\Application
 {
-
+    /**
+     * Determine if the application configuration is cached.
+     *
+     * @return bool
+     */
+    public function configurationIsCached(){
+        return false;
+    }
 }
